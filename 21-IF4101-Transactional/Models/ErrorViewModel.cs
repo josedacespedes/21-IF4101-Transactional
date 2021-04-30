ee97e52c2adedb3849ff858b62afd4be75efb472
José Céspedes <jose.alvaradocespedes@ucr.ac.cr>
2021-04-30 14:21:21 -0600

    first commit

---
+++ 21-IF4101-Transactional/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _21_IF4101_Transactional.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
