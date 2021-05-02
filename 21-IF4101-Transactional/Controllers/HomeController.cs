55d6ff104b7dd541857fa56f22cc62176fcdc2e3
Jenipher099 <jenn.mata.vrgs@gmailcom>
2021-05-01 22:44:07 -0600

    QD21-39 add Student to DB and verify StudentID

--- 21-IF4101-Transactional/Controllers/HomeController.cs
+++ 21-IF4101-Transactional/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using _21_IF4101_Transactional.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,8 @@ namespace _21_IF4101_Transactional.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        StudentDAO studentDAO;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +30,31 @@ namespace _21_IF4101_Transactional.Controllers
         {
             return View();
         }
+
+
+        public IActionResult Insert([FromBody] Student student)
+        {
+            //llamada al modelo para insertar el estudiante (ahora pasándole parámetro al constructor): 
+            studentDAO = new StudentDAO(_configuration);
+            int existToReturn = studentDAO.VerifyStudentID(student.StudentID);
+
+            if (existToReturn == 1)
+            {
+                return Ok(2);
+            }
+            else if (existToReturn == 0)
+            {
+                int resultToReturn = studentDAO.Insert(student); //acá guardamos un 1 o un 0, dependiendo de si se insertó el estudiante o no
+                return Ok(resultToReturn); //retornamos el 1 o el 0 a la vista
+            }
+            else
+            {
+                return Ok(existToReturn);
+            }
+
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ 21-IF4101-Transactional/Models/Data/StudentDAO.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _21_IF4101_Transactional.Models
+{
+
+    public class StudentDAO
+    {
+        private readonly IConfiguration _configuration;
+        string connectionString;
+
+        public StudentDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+            //esta es la llamada al string de conexión con la base de datos definido en el punto 3.
+        }
+
+        public StudentDAO()
+        {
+
+        }
+
+        public int Insert(Student student)
+        {
+            int resultToReturn; //declaramos variable que guardará un 1 o un 0 de acuerdo a si se insertó o no el student
+
+            SqlConnection connection = null;
+            try
+            {
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open(); //abrimos conexión
+                    SqlCommand command = new SqlCommand("InsertStudent", connection);
+                    
+                    command.CommandType = System.Data.CommandType.StoredProcedure; 
+
+                    command.Parameters.AddWithValue("@Name", student.Name);
+                    command.Parameters.AddWithValue("@Lastname", student.Lastname);
+                    command.Parameters.AddWithValue("@StudentID", student.StudentID);
+                    command.Parameters.AddWithValue("@Email", student.Email);
+                    command.Parameters.AddWithValue("@Password", student.Password);
+
+                    resultToReturn = command.ExecuteNonQuery(); //esta es la sentencia que ejecuta la inserción en BD y saca un 1 o un 0 dependiendo de si se modificó la tupla o no.Es decir, si se insertó en BD o no. 
+                    connection.Close(); //cerramos conexión. 
+                }
+                return resultToReturn;
+            }
+            catch (SqlException ex)
+            {
+                return ex.Number;
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        public int VerifyStudentID(string studentID)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("VerifyStudentID", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StudentID", studentID);
+
+                    var returnParameter = command.Parameters.Add("@Exists", SqlDbType.Int);
+                    returnParameter.Direction = ParameterDirection.ReturnValue;
+                    command.ExecuteNonQuery();
+
+                    int result = (int)returnParameter.Value;
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (SqlException ex)
+            {
+                return ex.Number;
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+
+        }
+
+    }
+}
