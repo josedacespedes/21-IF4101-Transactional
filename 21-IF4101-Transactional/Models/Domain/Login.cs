8b9de711f2680fa52a7a5b553673ac3bd5d44c79
WinstonAor <winkyortegar@hotmail.com>
2021-05-02 16:29:55 -0600

    QD21-43 Validate login credentials

---
+++ 21-IF4101-Transactional/Models/Domain/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _21_IF4101_Transactional.Models.Domain
+{
+    public class Login
+    {
+    }
+}
