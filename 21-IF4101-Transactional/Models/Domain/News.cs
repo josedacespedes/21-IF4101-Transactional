bfabd56ee99161e6d34e47900744b10d94e64835
Juan Diego Coto <jd506@hotmail.com>
2021-05-19 02:00:55 -0600

    QD21-135 Adding News Class

---
+++ 21-IF4101-Transactional/Models/Domain/News.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _21_IF4101_Transactional.Models.Domain
+{
+    public class News
+    {
+        private int id;
+        private string title;
+        private string description;
+        private string author;
+        private DateTime publication_Date;
+        private DateTime modification_Date;
+        private string file_New;
+        private string imagen;
+
+        public News()
+        {
+        }
+
+        public News(int id, string title, string description, string author, DateTime publication_Date, DateTime modification_Date, string file_New, string imagen)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Description = description;
+            this.Author = author;
+            this.Publication_Date = publication_Date;
+            this.Modification_Date = modification_Date;
+            this.File_New = file_New;
+            this.Imagen = imagen;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string Author { get => author; set => author = value; }
+        public DateTime Publication_Date { get => publication_Date; set => publication_Date = value; }
+        public DateTime Modification_Date { get => modification_Date; set => modification_Date = value; }
+        public string File_New { get => file_New; set => file_New = value; }
+        public string Imagen { get => imagen; set => imagen = value; }
+    }
+}
